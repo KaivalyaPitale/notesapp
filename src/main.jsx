5b157618cbb998f64072082fd84d6a9f64f20dc0
Kaivalya Pitale <pitale.4@osu.edu>
2025-09-21 23:10:25 -0400

    the notes app

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 
-import outputs from "./amplify_outputs.json"; // now in src/
 import App from "./App";
 import "./index.css";
 
-Amplify.configure(outputs);
+async function configureAmplify() {
+  const response = await fetch("/amplify_outputs.json");
+  const outputs = await response.json();
+  Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+configureAmplify();
